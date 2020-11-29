f0829459a73cb2c263801582bb9808e774573e53
raulGuru <basicallyrahulg@gmail.com>
2020-11-29 22:38:11 +0530

    added js functionality routing dashboard, campaigns, directory, login, pagenotfound app component adjustment added modules: layout , auth, Campaign, Dashboard, Directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PagenotfoundComponent } from './layout/pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  {
+    path: 'dashboard', 
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
+    path: 'campaigns', 
+    loadChildren: () => import('./campaign/campaign.module').then(m => m.CampaignModule)
+  },
+  {
+    path: 'directory', 
+    loadChildren: () => import('./directory/directory.module').then(m => m.DirectoryModule)
+  },
+  {
+    path: 'login', 
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
